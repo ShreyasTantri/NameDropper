f88c7576e833f47e0b88c4d6b684ff3855a062f3
ShreyasTantri <shreyastantrik29@gmail.com>
2026-01-30 08:03:01 +0530

    Initial Commit

---
+++ Name Dropper/Name_DropperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Name_DropperApp.swift
+//  Name Dropper
+//
+//  Created by CCS038 on 30/01/26.
+//
+
+import SwiftUI
+
+@main
+struct Name_DropperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
